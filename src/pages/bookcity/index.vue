3988f29b2ad64fc861a666772ba0b5b7248d9f62
Wang Tai <i@wangtai.me>
2023-08-29 17:46:46 +0800

    update catalog style

--- src/pages/bookcity/index.vue
+++ src/pages/bookcity/index.vue
@@ -8,41 +8,6 @@
           <div class="book-author">刘德华</div>
           <div class="book-title">今天也是你的新娘</div>
         </div>
-        <div class="book">
-          <div class="book-cover"></div>
-          <div class="book-author">刘德华</div>
-          <div class="book-title">今天也是你的新娘</div>
-        </div>
-        <div class="book">
-          <div class="book-cover"></div>
-          <div class="book-author">刘德华</div>
-          <div class="book-title">今天也是你的新娘</div>
-        </div>
-        <div class="book">
-          <div class="book-cover"></div>
-          <div class="book-author">刘德华</div>
-          <div class="book-title">今天也是你的新娘</div>
-        </div>
-        <div class="book">
-          <div class="book-cover"></div>
-          <div class="book-author">刘德华</div>
-          <div class="book-title">今天也是你的新娘</div>
-        </div>
-        <div class="book">
-          <div class="book-cover"></div>
-          <div class="book-author">刘德华</div>
-          <div class="book-title">今天也是你的新娘</div>
-        </div>
-        <div class="book">
-          <div class="book-cover"></div>
-          <div class="book-author">刘德华</div>
-          <div class="book-title">今天也是你的新娘</div>
-        </div>
-        <div class="book">
-          <div class="book-cover"></div>
-          <div class="book-author">刘德华</div>
-          <div class="book-title">今天也是你的新娘</div>
-        </div>
       </div>
     </div>
     <div class="suggest">
@@ -58,53 +23,9 @@
         <div class="scroll-wrap">
           <div class="suggest-menu-item active">都市言情</div>
           <div class="suggest-menu-item">都市言情</div>
-          <div class="suggest-menu-item">都市言情</div>
-          <div class="suggest-menu-item">都市言情</div>
-          <div class="suggest-menu-item">都市言情</div>
-          <div class="suggest-menu-item">都市言情</div>
-          <div class="suggest-menu-item">都市言情</div>
         </div>
       </div>
       <div class="suggest-list">
-        <div class="suggest-card">
-          <div class="suggest-card-main">
-            <div class="suggest-card-cover"></div>
-            <div class="suggest-card-info">
-              <div class="suggest-card-title">今天也是你的新娘</div>
-              <div class="suggest-card-desc">梦回好青年公寓，身边赫然出现的竟然是当年最最怀念的白色纱织礼服，抬眼望…</div>
-            </div>
-          </div>
-          <div class="suggest-footer">
-            <div class="suggest-footer-catalog">修仙玄幻</div>
-            <div class="suggest-footer-tips">更新至1230章</div>
-          </div>
-        </div>
-        <div class="suggest-card">
-          <div class="suggest-card-main">
-            <div class="suggest-card-cover"></div>
-            <div class="suggest-card-info">
-              <div class="suggest-card-title">今天也是你的新娘</div>
-              <div class="suggest-card-desc">梦回好青年公寓，身边赫然出现的竟然是当年最最怀念的白色纱织礼服，抬眼望…</div>
-            </div>
-          </div>
-          <div class="suggest-footer">
-            <div class="suggest-footer-catalog">修仙玄幻</div>
-            <div class="suggest-footer-tips">更新至1230章</div>
-          </div>
-        </div>
-        <div class="suggest-card">
-          <div class="suggest-card-main">
-            <div class="suggest-card-cover"></div>
-            <div class="suggest-card-info">
-              <div class="suggest-card-title">今天也是你的新娘</div>
-              <div class="suggest-card-desc">梦回好青年公寓，身边赫然出现的竟然是当年最最怀念的白色纱织礼服，抬眼望…</div>
-            </div>
-          </div>
-          <div class="suggest-footer">
-            <div class="suggest-footer-catalog">修仙玄幻</div>
-            <div class="suggest-footer-tips">更新至1230章</div>
-          </div>
-        </div>
         <div class="suggest-card">
           <div class="suggest-card-main">
             <div class="suggest-card-cover"></div>
@@ -129,7 +50,7 @@ import {ref} from 'vue'
 const title = ref('Hello')
 const jump = () => {
   uni.navigateTo({
-    url: '/pages/catalog/index'
+    url: '/pages/catalog/catalog'
   });
 };
 </script>
---
+++ src/pages/catalog/catalog.vue
@@ -0,0 +1,59 @@
+<template>
+  <view class="content">
+    <div class="suggest-menu">
+      <div class="scroll-wrap">
+        <div class="suggest-menu-item active">都市言情</div>
+        <div class="suggest-menu-item">都市言情</div>
+      </div>
+    </div>
+    <div class="book-list">
+      <div class="book">
+        <div class="cover"></div>
+        <div class="title">123</div>
+        <div class="tips">123</div>
+      </div>
+
+      <div class="book">
+        <div class="cover"></div>
+        <div class="title">123</div>
+        <div class="tips">123</div>
+      </div>
+
+      <div class="book">
+        <div class="cover"></div>
+        <div class="title">123</div>
+        <div class="tips">123</div>
+      </div>
+
+      <div class="book">
+        <div class="cover"></div>
+        <div class="title">123</div>
+        <div class="tips">123</div>
+      </div>
+
+      <div class="book">
+        <div class="cover"></div>
+        <div class="title">123</div>
+        <div class="tips">123</div>
+      </div>
+
+      <div class="book">
+        <div class="cover"></div>
+        <div class="title">123</div>
+        <div class="tips">123</div>
+      </div>
+
+    </div>
+
+  </view>
+</template>
+
+<script lang="ts" setup>
+import {ref} from 'vue'
+
+const title = ref('Hello')
+</script>
+
+<style lang="less">
+@import "catalog.less";
+</style>
